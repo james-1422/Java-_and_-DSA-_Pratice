cd9e4384a5a7888b50436bb60f14ad179b265735
StromMind_7 <jonessamueljs134@gmail.com>
2024-07-07 20:22:22 +0530

    7-7-24 DSA

--- DSA/Add.java
+++ DSA/Add.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Add {
+package DSA;
+
+public class Add {
+	static int add(int a, int b){
+		return a + b;
+	}
+	public static void main(String[] args) {
+		int ans = add(3,5);
+		System.out.println(ans);
+	}
 }
