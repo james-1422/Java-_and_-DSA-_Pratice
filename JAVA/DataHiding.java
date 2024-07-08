d0e5e24e2630c9850fabda325fab692d4a4857a4
James J <jamesjckm1019@gmail.com>
2024-07-08 17:48:55 +0530

    8-7-24 JAVA

---
+++ JAVA/DataHiding.java
@@ -0,0 +1,26 @@
+package JAVA;
+
+class person {
+    private int age;
+    private String name;
+
+    public person(int age, String name){
+        this.age=age;
+        this.name=name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getName(){
+        return name;
+    }
+
+}
+
+public class DataHiding {
+    public static void main(String[] args) {
+        person pr = new person(13,"jones");
+        System.out.println("Age:"+pr.getAge());
+        System.out.println("Name:"+pr.getName());
+    }
+}
