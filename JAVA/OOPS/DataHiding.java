a7c80f4510cf2a28b2cdaa8f9b465459ea542314
James J <jamesjckm1019@gmail.com>
2024-07-09 13:10:26 +0530

    9-7-24 Encapsulation

--- JAVA/OOPS/DataHiding.java
+++ JAVA/OOPS/DataHiding.java
@@ -16,11 +16,61 @@ class person{
     }
 
 }
+class Human{
+    private String gender;
+    private String name;
+    private int age;
+
+    public Human(String gender, String name,int age){
+        this.gender=gender;
+        this.name=name;
+        this.age=age;
+    }
+    public String getGender(){
+        return gender;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setGender(String gender){
+       this.gender=gender;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setAge(int age){
+        this.age=age;
+    }
+    public String toString() {
+        return name+ "\n"+ gender+ "\n"+ age ;
+    }
+}
 
 public class DataHiding {
     public static void main(String[] args) {
         person pr = new person(13,"jones");
         System.out.println("Age:"+pr.getAge());
         System.out.println("Name:"+pr.getName());
+
+        Human person1 = new Human("male","Jones",20);
+        System.out.println("Gender:"+person1.getGender());
+        System.out.println("Age:"+person1.getAge());
+        System.out.println("Name:"+person1.getName());
+        System.out.println("****************");
+       person1.setGender("Male");
+       person1.setName("Sam");
+       person1.setAge(21);
+        System.out.println("****************");
+        System.out.println("Gender:"+person1.getGender());
+        System.out.println("Age:"+person1.getAge());
+        System.out.println("Name:"+person1.getName());
+        System.out.println("****************");
+        System.out.println(person1.toString());
+
+
+
     }
 }
--- JAVA/OOPS/Encapsulation.java
+++ JAVA/OOPS/Encapsulation.java
@@ -1,2 +1,52 @@
-package JAVA.OOPS;public class Encapsulation {
+package JAVA.OOPS;
+class Employee{
+    private String empID;
+    private String name;
+    private double salary;
+
+    public Employee(String empID,String name,double salary){
+        this.empID=empID;
+        this.name=name;
+        this.salary=salary;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getEmpID() {
+        return empID;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+    public String toString(){
+        return empID +"\n"+ name +"\n"+ salary+"\n";
+    }
+
+    public void setEmpID(String empID) {
+        this.empID = empID;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+public class Encapsulation {
+    public static void main(String[] args) {
+        Employee epl = new Employee("101JS","Jones",100000);
+        System.out.println(epl.toString());
+        epl.setName("SAM");
+        epl.setEmpID("102SA");
+        epl.setSalary(5000000);
+        System.out.println(epl.toString());
+
+
+    }
+
 }
