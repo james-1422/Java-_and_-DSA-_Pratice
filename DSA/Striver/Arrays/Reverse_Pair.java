03d0e68636caa04683e6009dc604927a612ce878
StromMind_7 <jonessamueljs134@gmail.com>
2024-11-01 22:51:04 +0530

    01-11-24 Striver

---
+++ DSA/Striver/Arrays/Reverse_Pair.java
@@ -0,0 +1,16 @@
+package DSA.Striver.Arrays;
+
+public class Reverse_Pair {
+	public static int reversePairs(int[] nums) {
+		int ans =0;
+
+       return ans;
+	}
+
+	public static void main(String[] args) {
+		int[]nums={6,4,1,2,7};
+		int ans = reversePairs(nums);
+		System.out.println("Answer:"+ ans);
+
+	}
+}
